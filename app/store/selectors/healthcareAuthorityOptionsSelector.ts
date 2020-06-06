4675a0354401d220afd4de5104413076b32ba340
Jacob <JacobMaxJaffe@gmail.com>
2020-06-05 20:01:18 -0400

    disable eslint check

--- app/store/selectors/healthcareAuthorityOptionsSelector.ts
+++ app/store/selectors/healthcareAuthorityOptionsSelector.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
 const healthcareAuthorityOptionsSelector = (state: any) =>
   state.healthcareAuthorities.availableAuthorities;
 
